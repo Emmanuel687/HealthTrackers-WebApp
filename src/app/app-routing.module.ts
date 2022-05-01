ad7d56260955295560165d521de51b57de8e2da7
emmanuel687 <koechemmanuel2002@gmail.com>
2022-05-01 20:26:15 +0300

    Dev:Routes achieved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,15 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { LoginMoringaComponent } from './login/login-moringa/login-moringa.component';
 import { ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { LoginMoringaAdminComponent } from './login/login-moringa-admin/login-moringa-admin.component';
+import { LoginKemriAdminComponent } from './login/login-kemri-admin/login-kemri-admin.component';
 
 const routes: Routes = [
   {path:"home",component:HomeComponent},
   {path:"about",component:AboutComponent},
   {path:"driver",component:LoginDriverComponent},
+  {path:"moringaAdmin",component:LoginMoringaAdminComponent},
+  {path:"kemriAdmin",component:LoginKemriAdminComponent},
   
 
 
